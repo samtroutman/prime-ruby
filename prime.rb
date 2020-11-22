36e54370d176ac645bdd130e635d9be279ce15de
Sam <sam@Sams-MacBook-Air.local>
2020-11-22 15:14:32 -0500

    commit tree 7a2abb033c4372b8e511c464636a0227b3923022

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+    start = 2
+        if number > 1
+            range = (start..number-1).to_a
+            range.none? do |integer|
+                number % integer == 0 
+            end
+        else false
+        end
+end
